64424e90e3c38669922d6128582eb9377a19e5f5
dyl <dai00047@algonquinlive.com>
2022-10-05 14:15:24 -0400

    lab 4

--- app/src/main/java/ui/MainActivity.java
+++ app/src/main/java/ui/MainActivity.java
@@ -1,64 +1,33 @@
 package ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.util.Log;
 
-import algonquin.cst2335.dai00047.R;
+import androidx.appcompat.app.AppCompatActivity;
+
 import algonquin.cst2335.dai00047.databinding.ActivityMainBinding;
-import data.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imgView;
-    Switch sw;
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        imgView = findViewById(R.id.imageView);
-        sw = findViewById(R.id.switch1);
-
-        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
-
-            if (isChecked)
-            {
-                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-                rotate.setDuration(5000);
-                rotate.setRepeatCount(4);
-                rotate.setInterpolator(new LinearInterpolator());
-
-                imgView.startAnimation(rotate);
-            }
-            else {
-                imgView.clearAnimation();
-            }
-
-
-
-
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        String emailAddress = prefs.getString("emailEditText", "");
+        Log.e("MainActivity", "In onCreate()");
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.button.setOnClickListener(click -> {
+            Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
+            nextPage.putExtra("EmailAddress", binding.email.getText().toString());
+            prefs.edit().putString("Email", binding.email.getText().toString()).commit();
+            startActivity(nextPage);
         });
-
-
-
-
-
-
-
-
 
 
     }
---
+++ app/src/main/java/ui/SecondActivity.java
@@ -0,0 +1,16 @@
+package ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import algonquin.cst2335.dai00047.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
