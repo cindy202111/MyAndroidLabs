232caa031fb3f3d7fe568712b5ae5e9060ce1ec6
dyl <dai00047@algonquinlive.com>
2022-11-29 14:32:57 -0500

    week10 finished

--- app/src/main/java/ui/MainActivity.java
+++ app/src/main/java/ui/MainActivity.java
@@ -1,210 +1,161 @@
 package ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 
-import android.content.Context;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import algonquin.cst2335.dai00047.R;
-import algonquin.cst2335.dai00047.databinding.ActivityMainBinding;
-import data.MainViewModel;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
-/** @author Yongli Dai
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import algonquin.cst2335.dai00047.databinding.ActivityMainBinding;
+
+/**Description: Password validation for login
+ * @author Yongli Dai
  * @version 1.0
- * @since October 26, 2022
+ * @since  Nov.29,2022
  */
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding variableBinding;
-    private MainViewModel model;
-    /** This holds the login button reference     */
-    protected Button lButton;
-    protected TextView textView;
-
-    /** This holds the editText reference for login */
-    protected EditText loginText;
-
-
-    boolean foundUpperCase = false;
-    boolean foundLowerCase = false;
-    boolean foundNumber = false;
-    boolean foundSpecial = false;
+//    /**
+//     * This holds the text at the centre of the screen
+//     */
+//    private TextView tv = null;
+//    /**
+//     * This holds the editText reference for login
+//     */
+//    private EditText et = null;
+//    /**
+//     * This holds the login button reference
+//     */
+//    private Button btn = null;
 
 
 
+//    boolean foundUpperCase = false;
+//    boolean foundLowerCase = false;
+//    boolean foundNumber = false;
+//    boolean foundSpecial = false;
 
+    protected String cityName;
+    protected RequestQueue queue = null;
+    Bitmap image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //This part goes at the top of the onCreate function
+        queue = Volley.newRequestQueue(this);
+        ActivityMainBinding binding = ActivityMainBinding.inflate( getLayoutInflater() );
+        setContentView(binding.getRoot());
+
+        binding.forecastButton.setOnClickListener(click -> {
+            cityName = binding.cityTextField.getText().toString();
+            String stringURL = null;
+            try{
+                stringURL = "https://api.openweathermap.org/data/2.5/weather?q="
+                        + URLEncoder.encode(cityName, "UTF-8")
+                        + "&appid=7e943c97096a9784391a981c4d878b22&units=metric";
+
+                //this goes in the button click handler
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, stringURL, null,
+                        (response) -> {
+                            try {
+                                JSONObject mainObject = response.getJSONObject("main");
+                                double current = mainObject.getDouble("temp");
+                                double min = mainObject.getDouble("temp_min");
+                                double max = mainObject.getDouble("temp_max");
+                                int humidity = mainObject.getInt("humidity");
+
+                                JSONArray weather = response.getJSONArray("weather");
+                                JSONObject position = weather.getJSONObject(0);
+                                String description = position.getString("description");
+                                String icon = position.getString("icon");
+
+                                String pathname = getFilesDir() + "/" + icon + ".png";
+                                File file = new File(pathname);
+                                if(file.exists()){
+                                    image = BitmapFactory.decodeFile(pathname);
+
+                                }else {
+                                    ImageRequest imgReq = new ImageRequest("https://openweathermap.org/img/w/" + icon + ".png", new Response.Listener<Bitmap>() {
+
+                                        @Override
+                                        public void onResponse(Bitmap bitmap) {
+                                            // Do something with loaded bitmap
+                                            try {
+
+                                                image = bitmap;
+                                                image.compress(Bitmap.CompressFormat.PNG, 100, MainActivity.this.openFileOutput(icon + ".png", Activity.MODE_PRIVATE));
+
+                                            } catch (Exception e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    }, 1024, 1024, ImageView.ScaleType.CENTER, null, (error) -> {
+
+                                    });
+                                    queue.add(imgReq);
+                                }
+
+                                runOnUiThread( (  )  -> {
+                                    binding.temp.setText("The current temperature is " + current);
+                                    binding.temp.setVisibility(View.VISIBLE);
+
+                                    binding.minTemp.setText("The min temperature is " + min);
+                                    binding.minTemp.setVisibility(View.VISIBLE);
+
+                                    binding.maxTemp.setText("The max temperature is " + max);
+                                    binding.maxTemp.setVisibility(View.VISIBLE);
+
+                                    binding.humidity.setText("The humidity is " + humidity);
+                                    binding.humidity.setVisibility(View.VISIBLE);
+
+                                    binding.description.setText("The description is " + description);
+                                    binding.description.setVisibility(View.VISIBLE);});
+
+                                binding.icon.setImageBitmap(image);
+                                binding.icon.setVisibility(View.VISIBLE);
 
 
-        super.onCreate(savedInstanceState); //calls parent onCreate()
-       setContentView(R.layout.activity_main);
-        lButton = findViewById(R.id.button);
-        loginText = findViewById(R.id.editText);
-        textView = findViewById(R.id.textView);
-        stringHas123(loginText.getText().toString());
+
+                            }catch (JSONException e){
+                                e.printStackTrace();
+                            }
+                        },//success call back function
+                        // error call back function
+                        (error) -> {});
+
+                queue.add(request);
 
 
-        lButton.setOnClickListener(clk ->
 
-        {
-            String password = loginText.getText().toString();
-
-            if (checkPasswordComplexity(password)) {
-
-                textView.setText("Your password meets the requirements");
-
-            } else if (!foundUpperCase) {
-                Toast.makeText(this,"Your are missing an upper case letter",Toast.LENGTH_LONG).show();
-                textView.setText("You shall not pass!");
-
-            } else if (!foundLowerCase) {
-                Toast.makeText(this,"Your are missing a lower case letter",Toast.LENGTH_LONG).show();
-                textView.setText("You shall not pass!");
-
-            } else if (!foundNumber) {
-                Toast.makeText(this,"You are missing a digit",Toast.LENGTH_LONG).show();
-                textView.setText("You shall not pass!");
-
-            } else if (!foundSpecial) {
-                Toast.makeText(this,"You are missing a special character",Toast.LENGTH_LONG).show();
-                textView.setText("You shall not pass!");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
             }
-
-
 
         });
 
-
     }
-
-    /** This function checks if s has the string "123" somewhere in it
-     *
-     * @param s Is the string that we are checking for "123"
-     * @return Returns true if s has "123", false otherwise
-     */
-
-    public boolean stringHas123(String s){
-       return s.contains("123");
-    }
-
-
-
-
-    boolean isSpecialCharacter(char c){
-        switch (c){
-
-            case '#':
-            case '?':
-            case '*': return true;
-
-            default: return false;
-
-        }
-
-    }
-
-
-
-
-    /** This function
-     *
-     * @param pw The String object that we are checking
-     * @return Returns true if ....
-     */
-
-     public boolean checkPasswordComplexity(String pw) {
-
-    //     boolean foundUpperCase, foundLowerCase, foundNumber, foundSpecial;
-
-     //    foundUpperCase = foundLowerCase = foundNumber = foundSpecial = false;
-
-
-        // Aa check = loginText.getText().toString();
-
-
-         for (int i = 0; i < pw.length(); i++) {
-
-             char c = pw.charAt(i);
-
-            if(Character.isDigit(c)){foundNumber=true;}
-            else if(Character.isLowerCase(c)){foundLowerCase=true; }
-            else if(Character.isUpperCase(c)){ foundUpperCase=true;}
-            else if(isSpecialCharacter(c)){foundSpecial=true;}
-
-         }
-
-         if (!foundUpperCase) {
-           //  textView.setText("Your are missing an upper case letter");
-             return false;
-         }
-         else if(!foundLowerCase){
-          //   textView.setText("You are missing a lower case letter");
-             return false;
-         }
-
-         else if(!foundNumber ){
-         //    textView.setText("You are missing a digit");
-             return false;
-         }
-         else if(!foundSpecial){
-         //    textView.setText("You are missing special ");
-             return false;
-         }
-
-
-         return true;
-
-
-
-
-     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      //  model = new ViewModelProvider(this).get(MainViewModel.class);
-      //  variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
-      //  setContentView(variableBinding.getRoot()); // loads xml on screen
-
-
-//        variableBinding.mybutton.setOnClickListener(click ->{
-//            model.editString = variableBinding.myedittext.getText().toString();
-//            variableBinding.myedittext.setText("Your edit text has: "+model.editString);
-//
-//
-//        });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
